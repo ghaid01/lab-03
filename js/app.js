97238b936f6512378a00e4759c078d65e72d78e1
ghaid01 <ghaid.zahran2001@gmail.com>
2019-11-17 11:04:33 +0200

    lab03 done

--- js/app.js
+++ js/app.js
@@ -1,117 +1,115 @@
+'use strict'
+
 function Horns(data) {
-    this.image_url = data.image_url;
-    this.title = data.title;
-    this.description = data.description;
-    this.keyword = data.keyword;
-    this.horns = data.horns;
-    Horns.all.push(this);
-  }
-  Horns.all = [];
+  this.image_url = data.image_url;
+  this.title = data.title;
+  this.description = data.description;
+  this.keyword = data.keyword;
+  this.horns = data.horns;
+  Horns.all.push(this);
+}
+Horns.all = [];
+
+Horns.prototype.render = function () {
+  let getTemplate = $('#horns-template').html();
+  let template = Handlebars.compile(getTemplate);
+  console.log(this);
+  let hornOutput = template(this);
+  $('#photo-template').append(hornOutput);
   
-  Horns.prototype.render = function() {
+  console.log(this);
+};
+
+  //   let hornOutput = $('<div></div>');
+  //       hornOutput.addClass(this.keyword);
+  //   let template = $('#photo-template').html();
+  //   hornOutput.html( template );
+  //   hornOutput.find('h2').text( this.title );
+  //   hornOutput.find('img').attr('src', this.image_url);
+  //   hornOutput.find('p').text(this.description);
+
+  //   $('main').append(hornOutput);
+
   
-    // Create a new empty div tag
-    let hornOutput = $('<div></div>');
-        hornOutput.addClass(this.keyword);
-  
-    // clone (copy) the html from inside the photo-template
-    let template = $('#photo-template').html();
-  
-    // Add the template to the output div
-    hornOutput.html( template );
-  
-    // Put the data in
-    hornOutput.find('h2').text( this.title );
-    hornOutput.find('img').attr('src', this.image_url);
-    hornOutput.find('p').text(this.description);
-  
-    $('main').append(hornOutput);
-  
-  };
-  
+
   function populateSelectBox() {
     let seen = {};
     let select = $('select');
-    Horns.all.forEach( (horn) => {
-      if ( ! seen[horn.keyword] ) {
-        let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
+    $(select).empty();
+    Horns.all.forEach(horn => {
+      if (!seen[horn.keyword]) {
+        let option = `<option value ="${horn.keyword}">${horn.keyword}</option>`;
         select.append(option);
         seen[horn.keyword] = true;
       }
-    });
-  
-    console.log(seen);
+    })
   }
-  
-  $('select').on('change', function() {
+  function populateSortBox() {
+    $('.sort').on('change', function () {
+      if ($('.sort').val() == 'title') {
+        sortByTitle();
+        
+      } else if ($('.sort').val() == 'number') {
+        sortByNumOfHorns();
+      } else {
+
+      }
+    });
+  }
+  function sortByTitle() {
+    Horns.all.sort(function (a, b) {
+      var a = a.title
+      var b = b.title
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  function sortByHornsNum() {
+    Horns.all.sort(function (c, d) {
+      var c = c.horns
+      var d = d.horns
+      if (c < d) {
+        return -1;
+      }
+      if (c > d) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  $('#selecting').on('change', function () {
     let selected = $(this).val();
     $('div').hide();
     $(`.${selected}`).fadeIn(800);
   });
-function readData(){
-  $.get('../data/page-1.json')
-    .then( data => {
-      data.forEach( (thing) => {
-        let horn = new Horns(thing);
-        horn.render();
-      });
-       })
-       .then( () => populateSelectBox() );
 
-  }
-    function readData2(){
-    $.get('../data/page-2.json')
-    .then( data => {
-      data.forEach( (thing) => {
-        let horn = new Horns(thing);
-        horn.render();
-      });
-    })
-    // .then( () => populateSelectBox() );
-    }
-  
+  $('button').on('click' ,function () {
+    let page = $(this).attr('id');
+    console.log(page);
+     readData(page);
 
-// function Horns2(data){
-//       this.img_url = data.img_url;
-//       this.title = data.title;
-//       this.description = data.description;
-//       this.keyword = data.keyword;
-//       this.horns = data.horns;
-//       Horns2.all.push(this);
-
-//     }
+  });
+  function readData(pageNumber) {
+    $('#photo-template').empty();
     
-    // Horns2.all=[];
-
-
-// $.get('../data/page-2.json')
-//     .then( data => {
-//       data.forEach( (thing) => {
-//         let horn = new Horns(thing);
-//         horn.render();
-//       });
-//     })
-
-    $('#page-2').on('click', function(){
-      let g = $('nav').hide();
-      $(() => readData2());
-      let horn = new Horns(thing);
-      horn.render();
-      });
-      
-    //   $.get('../data/page-1.json')
-    // .then ( data => {
-    //   console.log(data)
-    //   data.forEach( (thing) => {
-    //     let horn = new Horns(thing);
-    //     horn.render();
-      
-    //   });
-    // })
-    $('#page-1').on('click', function(){
-    //  let g = $('nav').show();
-      $(() => readData());
-      let horn = new Horns(thing);
-      horn.render();
-      });
-      
+    Horns.all = [];
+    $.get(`../data/page-${pageNumber}.json`)
+      .then(data => {
+        
+        data.forEach((thing) => {
+          let horn = new Horns(thing)
+          horn.render();
+        });
+      })
+      .then(() => populateSelectBox())
+      .then(() => populateSortBox())
+  }
+$(document).ready(function () {
+    readData(1)
+});
